ec71ef4a76eb287a47f14cd6cd40e1b9684e7425
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-30 19:44:33 +0700

    menambahkan prosedur input

--- switchCase.cpp
+++ switchCase.cpp
@@ -16,37 +16,3 @@ void input() {
     cout << "Masukkan kode jurusan (1-3): ";
     cin >> kodeJurusan;
 }
-
-// prosedur untuk pengecekan kondisi
-void pilihanJurusan() {
-    switch (kodeJurusan) {
-        case 1:
-            namaJurusan = "Teknologi Informasi";
-            statusKuota = "Tersedia (15 Kursi)";
-            break;
-        case 2:
-            namaJurusan = "Teknologi Elektro";
-            statusKuota = "Kuota Penuh!";
-            break;
-        case 3:
-            namaJurusan = "Sistem Informasi";
-            statusKuota = "Tersedia (5 Kursi)";
-            break;
-        default:
-            namaJurusan = "Tidak Diketahui";
-            statusKuota = "Error: Kode Tidak Valid";
-    }
-}
-
-// prosedur output
-void output() {
-    cout << "\n=== HASIL SELEKSI ===" << endl;
-    cout << "Jurusan Pilihan : " << namaJurusan << endl;
-    cout << "Status Kuota    : " << statusKuota << endl;
-    cout << "----------------------------------" << endl;
-}
-
-int main() {
-    
-    return 0;
-}
